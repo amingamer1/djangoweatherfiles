2bc394d41433183aa32a5d71646f403a3377170c
Amin Seyedi <amingamer1@gmail.com>
2021-07-14 23:59:19 +0430

    views caption added

--- weather/lookup/views.py
+++ weather/lookup/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
+#url for index page
 def index(request):
     return render(request,'index.html',{})
-
+#url for about page
 def about(request):
     return render(request,'about.html',{})
